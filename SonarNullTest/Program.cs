b47adc48621c70231d4b2d46c2411d32b71998b9
OMINEXT\khoand <khoand@ominext.com>
2025-05-15 22:49:58 +0700

    First commit

---
+++ SonarNullTest/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        InspectType inspectType = new InspectType();
+        bool shouldDisable = true;
+
+        if (shouldDisable)
+        {
+            inspectType = null; // Simulate disable case
+        }
+
+        // This will cause NullReferenceException if inspectType is null
+        Console.WriteLine(inspectType.SomeProperty);
+    }
+}
+
+public class InspectType
+{
+    public string SomeProperty { get; set; } = "Test property";
+}
